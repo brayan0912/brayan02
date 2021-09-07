62df99182bd1c58b22b89cc96f49eac43397df42
brian rodriguez <bstevenrm12@gmail.com>
2021-09-07 16:06:32 -0500

    first commit

---
+++ brayan02.py
@@ -0,0 +1,8 @@
+anchura = int(input("Anchura del triangulo"))
+altura = int(input("Altura del triangulo"))
+
+for i in range(altura):
+    for j in range(anchura):
+        print("*", end="")
+    print()
+        
